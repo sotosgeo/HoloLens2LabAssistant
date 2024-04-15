3319bf659c9f88809405be6e4d9b3b5abeda974b
Sotiris Georgakopoulos <sotirisgeo99@gmail.com>
2024-04-15 12:40:50 +0300

    Addes tags, some more editing on component placement

--- Assets/MyScripts/PlacementManager.cs
+++ Assets/MyScripts/PlacementManager.cs
@@ -7,21 +7,11 @@ using UnityEngine;
 public class PlacementManager : MonoBehaviour
 {
     [SerializeField] GameObject[] placedComponentObjects;
-    [SerializeField] GameObject labBench;
+    
     private Collider _benchCollider;
     private bool localToggle = true;
 
 
-    private void Start()
-    {
-        _benchCollider = labBench.GetComponent<Collider>();
-    }
 
-    //Call when bench is placed in space
-    //
-    public void OnBenchPlacementFinished(bool forceToggle = true)
-    {
-        _benchCollider.enabled = forceToggle;
-
-    }
+    
 }
