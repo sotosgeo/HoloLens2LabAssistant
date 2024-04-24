9009eacd3f299d9f22925c317a5965825539f3d9
Sotiris Georgakopoulos <sotirisgeo99@gmail.com>
2024-04-24 15:54:15 +0300

    Moved trigger functionality to just cables

--- Assets/MyScripts/LabComponents/Pin.cs
+++ Assets/MyScripts/LabComponents/Pin.cs
@@ -10,40 +10,41 @@ public class Pin : MonoBehaviour
     [SerializeField] Material connectionFinalizedMaterial;
 
     public GameObject pinConnectedTo;
-    Coroutine collisionTimer;
     public string pinId;
 
-    private IEnumerator CollisionTimer(Collider other)
-    {
-        yield return new WaitForSeconds(ConnectionManager.connectionTime);
-        pinVisual.GetComponent<MeshRenderer>().material = connectionFinalizedMaterial;
-        Debug.Log("Collision Finalized with " + other.gameObject.GetComponent<Pin>().pinId);
-        pinConnectedTo = other.gameObject;
-    }
+
+    //Coroutine collisionTimer;
+    //private IEnumerator CollisionTimer(Collider other)
+    //{
+    //    yield return new WaitForSeconds(ConnectionManager.connectionTime);
+    //    pinVisual.GetComponent<MeshRenderer>().material = connectionFinalizedMaterial;
+    //    Debug.Log("Collision Finalized with " + other.gameObject.GetComponent<Pin>().pinId);
+    //    pinConnectedTo = other.gameObject;
+    //}
 
 
-    private void OnTriggerEnter(Collider other)
-    {
-        //When a collision is detected between this and another Collider 
-        Debug.Log("Collision Detected with " + other.gameObject.GetComponent<Pin>().pinId);
-        collisionTimer = StartCoroutine(CollisionTimer(other));
-        if (pinVisual != null)
-        {
-            pinVisual.GetComponent<MeshRenderer>().material = connectionDetectedMaterial;
-        }
-    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    //When a collision is detected between this and another Collider 
+    //    Debug.Log("Collision Detected with " + other.gameObject.GetComponent<Pin>().pinId);
+    //    collisionTimer = StartCoroutine(CollisionTimer(other));
+    //    if (pinVisual != null)
+    //    {
+    //        pinVisual.GetComponent<MeshRenderer>().material = connectionDetectedMaterial;
+    //    }
+    //}
 
 
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log("Collision Stopped with" + other.gameObject.GetComponent<Pin>().pinId);
-        pinVisual.GetComponent<MeshRenderer>().material = defaultMaterial;
-        if (collisionTimer != null)
-        {
-            StopCoroutine(collisionTimer);
-            collisionTimer = null;
-        }
-        pinConnectedTo = null;
-    }
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Debug.Log("Collision Stopped with" + other.gameObject.GetComponent<Pin>().pinId);
+    //    pinVisual.GetComponent<MeshRenderer>().material = defaultMaterial;
+    //    if (collisionTimer != null)
+    //    {
+    //        StopCoroutine(collisionTimer);
+    //        collisionTimer = null;
+    //    }
+    //    pinConnectedTo = null;
+    //}
 
 }
