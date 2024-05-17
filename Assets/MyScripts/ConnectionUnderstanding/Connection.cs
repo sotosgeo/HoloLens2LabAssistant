aa799450c35d1c8cbdff3f2d62eeeaf223dd83d4
Sotiris Georgakopoulos <sotirisgeo99@gmail.com>
2024-05-17 18:10:29 +0300

    Moved connection to seperate class

---
+++ Assets/MyScripts/ConnectionUnderstanding/Connection.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection : IEquatable<Connection>
+{
+
+    public string PinA { get; private set; }
+    public string PinB { get; private set; }
+    public int ConnectingCable { get; private set; }
+
+
+    public Connection(string pinA, string pinB, int connectingCable)
+    {
+        PinA = pinA;
+        PinB = pinB;
+        ConnectingCable = connectingCable;
+    }
+
+    public override string ToString()
+    {
+        return PinA.ToString() + " - > " + PinB.ToString() + " via cable " + ConnectingCable.ToString();
+    }
+
+
+    public bool Equals(Connection other)
+    {
+        return other != null && GetType() == other.GetType() && ((PinA == other.PinA && PinB == other.PinB) || (PinA == other.PinB && PinB == other.PinA));
+    }
+
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as Connection);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(PinA, PinB);
+    }
+}
--- Assets/MyScripts/ConnectionUnderstanding/ConnectionManager.cs
+++ Assets/MyScripts/ConnectionUnderstanding/ConnectionManager.cs
@@ -15,47 +15,7 @@ public class ConnectionManager : MonoBehaviour
 
     [SerializeField] TextMeshPro connectionText;
 
-
-    public class Connection : IEquatable<Connection>
-    {
-
-        public string PinA { get; private set; }
-        public string PinB { get; private set; }
-        public int ConnectingCable { get; private set; }
-
-
-        public Connection(string pinA, string pinB, int connectingCable)
-        {
-            PinA = pinA;
-            PinB = pinB;
-            ConnectingCable = connectingCable;
-        }
-
-        public override string ToString()
-        {
-            return PinA.ToString() + " - > " + PinB.ToString() + " via cable " + ConnectingCable.ToString();
-        }
-
-
-        public bool Equals(Connection other)
-        {
-            return other != null && GetType() == other.GetType() && ((PinA == other.PinA && PinB == other.PinB) || (PinA == other.PinB && PinB == other.PinA));
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as Connection);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(PinA, PinB);
-        }
-    }
-
-
-   
-
+        
 
     List<Connection> motorExcitementConnections = new List<Connection>()
         {
@@ -74,50 +34,45 @@ public class ConnectionManager : MonoBehaviour
         };
 
 
-    
+
 
     List<Connection> currentConnections = new();
 
     List<Connection> wrongConnections = new();
     List<Connection> missingConnections = new();
 
-    List<Connection> testConnections = new()
-    {
-        new Connection("networkPos","switchPosIn",5), //Should be correct
-        new Connection("resPos","switchPosOut",2), //Should be correct (just different order)
-        new Connection("amperIn","motorJ",0), //Should be incorrect
-        //all the others should be missing
-    };
+    //List<Connection> testConnections = new()
+    //{
+    //    new Connection("networkPos","switchPosIn",5), //Should be correct
+    //    new Connection("resPos","switchPosOut",2), //Should be correct (just different order)
+    //    new Connection("amperIn","motorJ",0), //Should be incorrect
+    //    //all the others should be missing
+    //};
     private void Start()
     {
         currentConnections.Clear();
         wrongConnections.Clear();
         missingConnections.Clear();
 
-       
+        Debug.Log(motorExcitementConnections.Contains(new("networkPos", "switchPosIn", 3)));
 
-        CheckConnection(testConnections);
+        //CheckConnection(testConnections);
 
-        Debug.Log("Wrong Connections are \n");
+    }
 
+    public void CheckCurrentConnection()
+    {
+        CheckConnection(currentConnections);
+
+
+        Debug.Log("Wrong connections are: ");
         TestPrintConnections(wrongConnections);
 
-        Debug.Log("Missing Connections are \n");
+       
 
+        Debug.Log("Missing Connections are: ");
         TestPrintConnections(missingConnections);
     }
-
-    private void OnEnable()
-    {
-
-    }
-
-    private void OnDisable()
-    {
-
-    }
-
-
 
     public void OnConnectionMade(string cableStart, string cableEnd, int cableId)
     {
@@ -162,6 +117,10 @@ public class ConnectionManager : MonoBehaviour
 
     public void CheckConnection(List<Connection> connectionsToCheck)
     {
+
+        wrongConnections.Clear();
+        missingConnections.Clear();
+
         //Find all the wrong Connections
         foreach (var connection in connectionsToCheck)
         {
@@ -174,16 +133,16 @@ public class ConnectionManager : MonoBehaviour
 
         }
 
-        if(motorExcitementConnections.Any())
+        if (motorExcitementConnections.Any())
 
-        //Find the missing Connections
-        foreach (var connection in motorExcitementConnections)
-        {
-            if (!connectionsToCheck.Contains(connection))
+            //Find the missing Connections
+            foreach (var connection in motorExcitementConnections)
             {
-                missingConnections.Add(connection);
+                if (!connectionsToCheck.Contains(connection))
+                {
+                    missingConnections.Add(connection);
+                }
             }
-        }
     }
 
 
