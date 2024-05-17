7dfe13e5dc9691ea9978d55ce2d7044342a7b91f
Sotiris Georgakopoulos <sotirisgeo99@gmail.com>
2024-05-17 12:53:08 +0300

    fixed? rotation handles, connection Check
    Added missing and wrong connection searching, now I need to implement a way to highlight these wrong connections to the student.

--- Assets/MyScripts/ConnectionUnderstanding/ConnectionManager.cs
+++ Assets/MyScripts/ConnectionUnderstanding/ConnectionManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using System;
 using System.ComponentModel.Design;
 using TMPro;
+using Mirror;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 
 public class ConnectionManager : MonoBehaviour
@@ -36,7 +39,7 @@ public class ConnectionManager : MonoBehaviour
 
         public bool Equals(Connection other)
         {
-            return other != null && GetType() == other.GetType() && PinA == other.PinA && PinB == other.PinB;
+            return other != null && GetType() == other.GetType() && ((PinA == other.PinA && PinB == other.PinB) || (PinA == other.PinB && PinB == other.PinA));
         }
 
         public override bool Equals(object obj)
@@ -50,18 +53,58 @@ public class ConnectionManager : MonoBehaviour
         }
     }
 
+
+   
+
+
     List<Connection> motorExcitementConnections = new List<Connection>()
         {
-            new Connection("motorJ","amperOut",0),
+            new Connection("networkPos","switchPosIn",0),
+            new Connection("networkNeg","switchNegIn",0),
+
+            new Connection("switchPosOut","resPos",0),
+            new Connection("switchNegOut","resNeg",0),
+
+            new Connection("resPos","amperIn",0),
+            new Connection("resGnd","motorK",0),
+
+            new Connection("amperOut","motorJ",0),
+
 
         };
 
+
+    
+
     List<Connection> currentConnections = new();
+
+    List<Connection> wrongConnections = new();
+    List<Connection> missingConnections = new();
+
+    List<Connection> testConnections = new()
+    {
+        new Connection("networkPos","switchPosIn",5), //Should be correct
+        new Connection("resPos","switchPosOut",2), //Should be correct (just different order)
+        new Connection("amperIn","motorJ",0), //Should be incorrect
+        //all the others should be missing
+    };
     private void Start()
     {
-
         currentConnections.Clear();
+        wrongConnections.Clear();
+        missingConnections.Clear();
 
+       
+
+        CheckConnection(testConnections);
+
+        Debug.Log("Wrong Connections are \n");
+
+        TestPrintConnections(wrongConnections);
+
+        Debug.Log("Missing Connections are \n");
+
+        TestPrintConnections(missingConnections);
     }
 
     private void OnEnable()
@@ -74,7 +117,7 @@ public class ConnectionManager : MonoBehaviour
 
     }
 
-  
+
 
     public void OnConnectionMade(string cableStart, string cableEnd, int cableId)
     {
@@ -95,14 +138,51 @@ public class ConnectionManager : MonoBehaviour
         PrintConnections();
     }
 
-
     void PrintConnections()
     {
         connectionText.text = "";
         foreach (var connection in currentConnections)
         {
-            
+
             connectionText.text += connection.ToString() + '\n';
+        }
+
+
+
+    }
+
+
+    void TestPrintConnections(List<Connection> connections)
+    {
+        foreach (var connection in connections)
+        {
+            Debug.Log(connection.ToString());
+        }
+    }
+
+    public void CheckConnection(List<Connection> connectionsToCheck)
+    {
+        //Find all the wrong Connections
+        foreach (var connection in connectionsToCheck)
+        {
+            if (!motorExcitementConnections.Contains(connection))
+            {
+                wrongConnections.Add(connection);
+            }
+
+
+
+        }
+
+        if(motorExcitementConnections.Any())
+
+        //Find the missing Connections
+        foreach (var connection in motorExcitementConnections)
+        {
+            if (!connectionsToCheck.Contains(connection))
+            {
+                missingConnections.Add(connection);
+            }
         }
     }
 
